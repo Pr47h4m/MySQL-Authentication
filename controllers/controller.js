675c5f55360e06b1aa5a0b2797a3066838e6071c
Pratham Jaiswal <dev.pr47h4m@gmail.com>
2021-02-20 17:49:28 +0530

    basic validation

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,17 +1,30 @@
 const express = require("express");
 
 const API_KEY = require("../secrets/api-keys");
+const User = require("../models/user");
 
 const validateRequest = (req, res, next) => req.query.key == API_KEY;
-
 exports.createUser = (req, res, next) => {
   if (!validateRequest(req))
     return res.send({ error: { message: "Invalid API_KEY" } });
   const body = req.body;
+  User.create({ email: body.email, password: body.password })
+    .then(() => {
+      res.send({ status: "success" });
+    })
+    .catch((err) => console.error(err));
 };
 
 exports.signInUser = (req, res, next) => {
   if (!validateRequest(req))
     return res.send({ error: { message: "Invalid API_KEY" } });
   const body = req.body;
+  User.findAll({ where: { email: body.email } })
+    .then((users) => {
+      const user = users[0];
+      if (user.password != body.password)
+        return res.send({ error: { message: "Invalid Credentials" } });
+      res.send({ status: "success" });
+    })
+    .catch((err) => console.error(err));
 };
