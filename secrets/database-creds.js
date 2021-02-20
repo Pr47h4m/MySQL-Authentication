e3158908f33cef97abc8b8be1bac95e170f08dfb
Pratham Jaiswal <dev.pr47h4m@gmail.com>
2021-02-20 17:29:28 +0530

    changed database secrets

--- secrets/database-creds.js
+++ secrets/database-creds.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host: '',
+    host: 'bg5a2xxn60ckgadegl8l-mysql.services.clever-cloud.com',
     port: 3306, 
-    user: '',
-    database: '',
-    password: ''
+    user: 'uvu8bztqqzbw0fcb',
+    database: 'bg5a2xxn60ckgadegl8l',
+    password: 't7v0DHwsY9tzTEnNOHxK'
 }
